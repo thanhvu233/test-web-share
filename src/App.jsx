ff7d56ac5e6090c6593b2309975c0760a9247b98
thanhvt2 <thanhvt2@rikkeisoft.com>
2024-11-29 10:46:44 +0700

    init project

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from 'react'
+import './App.css'
+import Share from './Share'
+
+function App() {
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetch("https://picsum.photos/200")
+    .then(function(response) {
+      return response.blob()
+    })
+    .then(function(blob) {
+
+      const file = new File([blob], "picture.jpg", {type: 'image/jpeg'});
+      
+      setImages([file]);
+    })
+  }, [])
+  
+
+  return (
+    <div className="App">
+      <Share
+      label="Share"
+      title="My Web Share Adventures"
+      text="Hello World! I shared this content via Web Share"
+      files={images}
+    />
+    </div>
+  )
+}
+
+export default App
---
+++ src/Share.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+function Share({ label, text, title, files }) {
+  const canonical = document.querySelector("link[rel=canonical]");
+  let url = canonical ? canonical.href : document.location.href;
+  const shareDetails = { url, title, text, files };
+
+  const handleSharing = async () => {
+    if (navigator.canShare) {
+      try {
+        await navigator
+          .canShare(shareDetails)
+          .then(() =>
+            console.log("Hooray! Your content was shared to tha world")
+          );
+      } catch (error) {
+        console.log(`Oops! I couldn't share to the world because: ${error}`);
+      }
+    } else {
+      // fallback code
+      console.log(
+        "Web share is currently not supported on this browser. Please provide a callback"
+      );
+    }
+  };
+  return (
+    <button onClick={handleSharing}>
+      <span>{label}</span>
+    </button>
+  );
+}
+export default Share;
